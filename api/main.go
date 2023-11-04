f16e8ff940760a5826f6641672f92ceb1be3b7b3
samdoescode <kennedyhine@gmail.com>
2023-11-04 18:00:03 +1100

    starting to work on golang ollama integration

--- api/main.go
+++ api/main.go
@@ -3,8 +3,28 @@ package handler
 import (
   "fmt"
   "net/http"
+	"bytes"
+	"io"
+	"log"
+	"os"
 )
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+body := []byte(`{"model":"mistral"}`)
+	resp, err := http.Post("http://localhost:11434/api/generate", "application/json", bytes.NewBuffer(body))
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  fmt.Fprintf(w, string(responseData))
+
 }
+
+
